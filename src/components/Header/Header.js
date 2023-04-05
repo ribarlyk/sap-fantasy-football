932d059a2f72be0fbf7991bff2d2970d95ffa072
Pavel Hristov <hristov.pavel@zohomail.eu>
2023-04-05 17:31:17 +0300

    header navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,22 @@
-import ClubBadges from "../Badges/ClubBadges"
-import ResponsiveAppBar from "../Navigation/Navigation"
-import logo from "../../assets/images/premier-league-logo.jpg"
+import ClubBadges from "../Badges/ClubBadges";
+import ResponsiveAppBar from "../Navigation/Navigation";
+import logo from "../../assets/images/premier-league-logo.jpg";
 
-import "./Header.css"
+import "./Header.css";
 export default function Header() {
     return (
         <header className="header">
-            <ClubBadges />
-            <img src={logo} width="250px" height="200px" alt="" className="pl-logo" />
-            <ResponsiveAppBar />
+            <img
+                src={logo}
+                width="250px"
+                height="200px"
+                alt=""
+                className="pl-logo"
+            />
+            <div className="links-container">
+                <ClubBadges />
+                <ResponsiveAppBar />
+            </div>
         </header>
-    )
+    );
 }
