dbc9b705dfaa37465a4afbf34d874fc0cbd28531
Pavel Hristov <hristov.pavel@zohomail.eu>
2023-04-12 21:28:51 +0300

    login redone

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,15 +1,15 @@
 import "./Login.scss";
 import { motion } from "framer-motion";
-import LoginForm from "./LoginForm";
+import  SignInSide  from "../Login/LoginComponent";
+
 export default function Login() {
     return (
-        <motion.div
-            className="login-container"
+        <motion.div className="login-container"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
         >
-            <LoginForm />
+            <SignInSide/>
         </motion.div>
     );
 }
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -4,12 +4,12 @@ import SignUpSide from "./RegisterComponent";
 
 export default function Register() {
     return (
-        <motion.div className="register-container"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-        >
+        // <motion.div className="register-container"
+        //     initial={{ opacity: 0 }}
+        //     animate={{ opacity: 1 }}
+        //     exit={{ opacity: 0 }}
+        // >
+        // </motion.div>
             <SignUpSide/>
-        </motion.div>
     );
 }
